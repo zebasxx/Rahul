645487b4bb0c941bdda50bca9bf9806d70181a44
Sebastian Garcia <zebasxx@hotmail.com>
2020-04-22 17:31:27 -0300

    Adding Move Completed Files Script to Repo

---
+++ Move Completed Files/ScanFolderForCompleted.ps1
@@ -0,0 +1,18 @@
+﻿#Set the destination pathe below:
+$PathToScan = 'C:\HomeFolder'
+
+Function Scan-Folder {
+    param([Object[]] $FileList)
+    foreach ($File in $FileList)
+    {
+        if ($File.Name -match 'completed' -and (-not ($File.Directory.FullName -match 'completed')) )
+        {
+            $TimeStamp = (Get-Date).tostring("MM-dd-yyyy-hh-mm")
+            $Destination = $File.Directory.FullName+'\Completed\'+$File.BaseName+'_'+$TimeStamp+$File.Extension
+            Move-Item -Path $File.FullName -Destination $Destination
+        }
+    }
+}
+
+$Files = Get-ChildItem -Path $PathToScan -File -Recurse
+Scan-Folder $Files
